e99625a13838fc6c03fbfa25881df762b81af1d9
an4umov <an4umov@gmail.com>
2020-01-30 13:50:19 +0300

    timer num fix, end

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //Timer
 
-    let deadLine = '2020-02-05';
+    let deadLine = '2020-02-14';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -68,12 +68,22 @@ window.addEventListener('DOMContentLoaded', function() {
 
         function updateClock() {
             let t = getTimeRemaining(endtime);
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
+
+            function addZero(num){
+                if(num <= 9) { 
+                    return '0' + num;
+                }   else return num;
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
             }
         }    
     }
