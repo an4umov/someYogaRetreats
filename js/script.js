46605b6f3f8b88c75576b39b841dfbe1ad3b0a32
an4umov <an4umov@gmail.com>
2020-01-30 13:35:28 +0300

    +timer

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
     // Tabs
-    
+
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -38,4 +38,45 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    //Timer
+
+    let deadLine = '2020-02-05';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            // hours = Math.floor((t/(1000*60*60))),
+            // days = Math.floor((t/(1000*60*60*24))),
+            hours = Math.floor((t/1000/60/60) % 24);
+
+            return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+            
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }    
+    }
+
+    setClock('timer', deadLine);
 });
